06cb3416f5eee698e963c7c8ed6b7ef5df824fcd
Peter <ps@dehr.com>
2016-04-06 13:42:12 -0400

    Initial check-in.

---
+++ SqlServerExpressCheck/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+
+namespace SqlServerExpressCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myServiceName = "MSSQL$SQLEXPRESS";
+            string status;
+
+            Console.WriteLine("Service: " + myServiceName);
+
+            ServiceController mySc = new ServiceController(myServiceName);
+
+            // Try to get the service status
+            try
+            {
+                status = mySc.Status.ToString();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Service not found.  It is probably not installed. \nException: " + e.Message);
+                Console.ReadLine();
+            }
+
+            // Start the service if it was stopped
+            if (mySc.Status.Equals(ServiceControllerStatus.Stopped) | mySc.Status.Equals(ServiceControllerStatus.StopPending))
+            {
+                try
+                {
+                    Console.WriteLine("Starting the service...");
+                    mySc.Start();
+                    mySc.WaitForStatus(ServiceControllerStatus.Running);
+                    Console.WriteLine("The service is now " + mySc.Status.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error in starting the service: " + e.Message);
+                }
+            }
+
+            Console.WriteLine("Press a key to end the app...");
+            Console.ReadLine();
+            return;
+        }
+    }
+}
